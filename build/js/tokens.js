2c8d0ab8243fcc80167bdf031311c958ccf2da54
Bruno Lima <blima@ane.local>
2023-05-10 15:38:01 -0300

    first commit

---
+++ build/js/tokens.js
@@ -0,0 +1,13 @@
+/**
+ * Do not edit directly
+ * Generated on Wed, 10 May 2023 18:12:31 GMT
+ */
+
+module.exports = {
+  "ColorWhite": "#ffffff",
+  "ColorGrey200": "#c1c1c4",
+  "ColorGrey500": "#50505c",
+  "ColorGrey800": "#1c1c1f",
+  "ColorGrey900": "#121214",
+  "ColorPurplePurple": "#8b5cf6"
+};
