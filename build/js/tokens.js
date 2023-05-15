ed47562c47b5092a047669f550a0a16f82632887
brunblima <brunblima@users.noreply.github.com>
2023-05-15 17:33:02 +0000

    style: Update design tokens

--- build/js/tokens.js
+++ build/js/tokens.js
@@ -1,13 +1,17 @@
 /**
  * Do not edit directly
- * Generated on Wed, 10 May 2023 18:12:31 GMT
+ * Generated on Mon, 15 May 2023 17:33:01 GMT
  */
 
 module.exports = {
+  "Sizes2": 8,
+  "Sizes4": 16,
+  "Sizes8": 32,
   "ColorWhite": "#ffffff",
   "ColorGrey200": "#c1c1c4",
   "ColorGrey500": "#50505c",
   "ColorGrey800": "#1c1c1f",
   "ColorGrey900": "#121214",
+  "ColorPurple400": "#9874f3",
   "ColorPurplePurple": "#8b5cf6"
 };
